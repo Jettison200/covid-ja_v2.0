5efcb06c0ac5855aa64099e58ad86b06d196d3e8
Jettison200 <jettison200@gmail.com>
2020-05-05 21:34:00 -0500

    version2.0, a new look and feel

--- index.js
+++ index.js
@@ -1,5 +1,41 @@
-/*
+
 if ('scrollRestoration' in history) {
   history.scrollRestoration = 'manual';
 }
-*/
+document.getElementById("hamburger").onclick = reveal;
+
+let toggle = 0;
+
+//*Functions
+
+function reveal() {
+  let hamburger = document.getElementById("hamburger");
+  let sideNav = document.getElementById("sidenav");
+  if (toggle == 0) {
+    hamburger.classList.add("is-active");
+    toggle++;
+  } else {
+    hamburger.classList.remove("is-active");
+    toggle--;
+  }
+}
+
+
+function remove() {
+  var myobj = document.getElementById("a-1");
+  var myobj2 = document.getElementById("a-2");
+  myobj.remove();
+  myobj2.remove();
+}
+
+function myFunction(x) {
+  if (x.matches) { // If media query matches
+    remove()
+  } else {
+
+  }
+}
+var x = window.matchMedia("(max-width: 700px)")
+myFunction(x) // Call listener function at run time
+x.addListener(myFunction) // Attach listener function on state changes
+
