378832bd32f3adf17b5bcd1be2fedc127d7da106
Jettison200 <jettison200@gmail.com>
2020-05-06 22:44:00 -0500

    corrections

--- index.js
+++ index.js
@@ -5,29 +5,7 @@ if ('scrollRestoration' in history) {
 
 
 
-//*Remove Animation from html
 
-function remove() {
-  var myobj = document.getElementById("a-1");
-  var myobj2 = document.getElementById("a-2");
-  myobj.remove();
-  myobj2.remove();
-}
-
-function myFunction(x) {
-  if (x.matches) {
-    remove()
-  } else {
-
-  }
-}
-var x = window.matchMedia("(max-width: 700px)")
-myFunction(x) // Call listener function at run time
-x.addListener(myFunction) // Attach listener function on state changes
-
-
-
-//*Remove loader
 
 
 let tl = gsap.timeline();
