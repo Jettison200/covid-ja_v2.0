4c2913be15763677fa231658461d21ab5f73401b
Jettison200 <jettison200@gmail.com>
2020-07-16 18:32:35 -0500

    Version 3 with Suniels React App

---
+++ precache-manifest.ebb6510a6acebfd4037df0014e050a42.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51666701e41ef212cd3217c30083f26b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "67954d0af464675faf30",
+    "url": "/static/css/main.9614925c.chunk.css"
+  },
+  {
+    "revision": "9eca79c70abee10c4863",
+    "url": "/static/js/2.4c5006d3.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/static/js/2.4c5006d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67954d0af464675faf30",
+    "url": "/static/js/main.ee6a4f08.chunk.js"
+  },
+  {
+    "revision": "3ff5d6fb19a2b7a4acf2",
+    "url": "/static/js/runtime-main.8b39ddf8.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/static/media/virus.d7265326.png"
+  }
+]);
